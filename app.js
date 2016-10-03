587c6f1d1da45fbcf5a7b56a587d21b02a675886
Romain <RomainFrechic@outlook.fr>
2016-10-03 16:48:08 +0200

    greeting

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('have fun !');
+
+
+
+
+
+
+var form = {
+    first_name: " ",
+    last_name: " ",
+    city: " "
+}
+function main() {
+    $("#button").click(function() {
+        form["first_name"] = $("#first_name").val();
+        form["last_name"] = $("#last_name").val();
+        form["city"] = $("#city").val();
+        $("#username").text(form["first_name"] + form["last_name"]);
+        console.log(form);
+    });
+}
+main();
