09447995add8039b2cd003217f6d9ff1d2241cf3
Amine EL G <aelghayate@simplon.co>
2016-10-07 13:33:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 console.log('have fun !');
+var form = {};
 
-
-
-
-
-
-var form = {
-    first_name: " ",
-    last_name: " ",
-    city: " "
-}
 function main() {
     $("#button").click(function() {
-        form["first_name"] = $("#first_name").val();
-        form["last_name"] = $("#last_name").val();
-        form["city"] = $("#city").val();
-        $("#username").text(form["first_name"] + form["last_name"]);
+        form.first_name = $("#first_name").val();
+        form.last_name = $("#last_name").val();
+        form.city = $("#city").val();
+        
+        $("#username").text(form["first_name"] +' '+ form["last_name"]);
         console.log(form);
     });
 }
